f580e8c6ebbe64fc662012b9dac6a26535bd305f
Razafimanantsoa Johnson <johnsonrazafimanantsoa@gmail.com>
2024-11-20 21:25:09 +0400

    Second Chapter done

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock= pygame.time.Clock()
     dt= 0
+    updatables= pygame.sprite.Group()
+    drawables= pygame.sprite.Group()
+    Player.containers= (updatables, drawables) 
     player= Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -16,11 +19,15 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        
+        for updatable in updatables:
+            updatable.update(dt)
+        
+        for drawable in drawables:
+            drawable.draw(screen)
 
         pygame.display.flip()
         dt= clock.tick(60)/1000
-        print(dt)
 
 if __name__=="__main__":
     main()
